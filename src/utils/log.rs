134259b65141e5fa19db97031647224402204214
vladigris <vladigris@gmail.com>
2026-03-23 21:35:15 +0300

    Update logger

--- src/utils/log.rs
+++ src/utils/log.rs
@@ -3,7 +3,7 @@ use std::io::Write;
 
 pub fn init_logger() {
     env_logger::Builder::new()
-        .format(|buf, record| writeln!(buf, "{}", record.args()))
+        .format(|buf, record| writeln!(buf, "{}: {}", record.level(), record.args()))
         .filter(None, LevelFilter::Info)
         .init();
 }
